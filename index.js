8b811e62c27a00e0820a8de890d4fef6a35193d6
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-10 14:39:20 +0900

    Print received parameters

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 'use strict';
 
+const REQUIRED_KEYS = ['cluster', 'taskDefinition', 'commands'];
+
+let AWS = require('aws-sdk');
+let ecs = new AWS.ECS();
+
+function validateEvent(event) {
+  let missingKeys = [];
+
+  REQUIRED_KEYS.forEach(key => {
+    if (event[key] == null) {
+      missingKeys.push(key);
+    }
+  });
+
+  return missingKeys;
+}
+
 exports.handler = (event, context, callback) => {
+  let missingKeys = validateEvent(event);
+
+  if (missingKeys.length > 0) {
+    callback('missing keys: ' + missingKeys.toString());
+    return;
+  }
+
+  let cluster = event['cluster'];
+  let taskDefinition = event['taskDefinition'];
+  let commands = event['commands'];
+
+  console.log(ecs);
+  console.log('cluster: ' + cluster);
+  console.log('taskDefinition: ' + taskDefinition);
+  console.log('commands: ' + commands);
+
   console.log(event);
   callback(null, 'ok');
 };
